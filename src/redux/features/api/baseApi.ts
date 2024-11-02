44e281b4fcc4ea8d5b2cc936b7eea957104bf498
apucsd <apusutradhar77@gmail.com>
2024-11-02 11:45:45 +0600

    deploy project in vercel

--- src/redux/features/api/baseApi.ts
+++ src/redux/features/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseApi = createApi({
       reducerPath: 'baseApi',
-      baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/api' }),
+      baseQuery: fetchBaseQuery({ baseUrl: 'https://baby-food-frontend-sage.vercel.app/api' }),
       endpoints: () => ({}),
       tagTypes: ['products', 'faqs', 'blogs', 'supports'],
 });
