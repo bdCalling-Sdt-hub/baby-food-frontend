81664bc927c6836f0d202047c8c2333b3bebd7d0
apucsd <apusutradhar77@gmail.com>
2024-11-09 17:24:25 +0600

    changes base api url

--- src/redux/features/api/baseApi.ts
+++ src/redux/features/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseApi = createApi({
       reducerPath: 'baseApi',
-      baseQuery: fetchBaseQuery({ baseUrl: 'http://92.205.29.166:3000' }),
+      baseQuery: fetchBaseQuery({ baseUrl: 'http://92.205.29.166:3000/api' }),
       endpoints: () => ({}),
       tagTypes: ['products', 'faqs', 'blogs', 'supports'],
 });
