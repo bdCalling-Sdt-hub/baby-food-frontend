0d98e608c07ccb8bd294ecf53c533a189b5cfcfa
apucsd <apusutradhar77@gmail.com>
2024-11-09 17:02:13 +0600

    added new base url

--- src/redux/features/api/baseApi.ts
+++ src/redux/features/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseApi = createApi({
       reducerPath: 'baseApi',
-      baseQuery: fetchBaseQuery({ baseUrl: 'https://baby-food-frontend-sage.vercel.app/api' }),
+      baseQuery: fetchBaseQuery({ baseUrl: 'http://92.205.29.166:3000' }),
       endpoints: () => ({}),
       tagTypes: ['products', 'faqs', 'blogs', 'supports'],
 });
