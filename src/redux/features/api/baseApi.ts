c6ec8c13313beed2b771061c147fa53d437d9fe2
apucsd <apusutradhar77@gmail.com>
2024-11-26 15:22:01 +0600

    updated the url

--- src/redux/features/api/baseApi.ts
+++ src/redux/features/api/baseApi.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const baseApi = createApi({
       reducerPath: 'baseApi',
-      baseQuery: fetchBaseQuery({ baseUrl: 'http://92.205.29.166:3000/api' }),
+      baseQuery: fetchBaseQuery({ baseUrl: 'https://covevalleyorganics.com/api' }),
       // http://92.205.29.166:3000/
       endpoints: () => ({}),
       tagTypes: ['products', 'faqs', 'blogs', 'supports'],
